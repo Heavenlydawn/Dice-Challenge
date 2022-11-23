552ae19d70dd18695e1c49d52ee751b1edfb09fe
Heavenlydawn <gabrielheavenlydawn@gmail.com>
2022-11-23 18:03:40 +0100

    JS Modification

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
 
-var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomDiceImage = "dice" + randomNumber1 + ".png"; 
 
-var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var randomImageSource = "images/" + randomDiceImage;
 
 var image1 = document.querySelectorAll("img")[0];
 
@@ -21,9 +21,11 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
+// If player 2 wins
 else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
+// If it's a Draw
 else {
   document.querySelector("h1").innerHTML = "Draw!";
 }
